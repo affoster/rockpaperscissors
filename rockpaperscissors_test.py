8b6f30f65527c536d0ede65d175761ae9b563b84
Allison Foster <allisonffoster@yahoo.com>
2020-05-05 00:37:18 -0400

    pytest setup complete

---
+++ rockpaperscissors_test.py
@@ -0,0 +1,9 @@
+# pylint: disable=unused-variable
+"""Unit tests for RockPaperScissors."""
+
+def describe_a_rockpaperscissors_program_that():
+    """A program to play Rock Paper Scissors."""
+
+    def has_a_smoke_test():
+        """Make sure our testing infrastructure is working."""
+        assert True == True
